00b62bae34779a481f3c4a37b16c165b0f47f967
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-16 18:58:43 -0700

    installed babel runtime package,  app bar now working

--- the_venue/src/components/header_footer/header.js
+++ the_venue/src/components/header_footer/header.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
-import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/core/Menu';
+// import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
     render() {
