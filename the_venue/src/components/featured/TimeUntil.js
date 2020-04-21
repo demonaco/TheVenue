32ba945c9e192bc948e9c75a8844dc9b40b3ffbe
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-20 19:23:23 -0700

    adding TimeUntil component.

---
+++ the_venue/src/components/featured/TimeUntil.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class TimeUntil extends Component {
+    state = {
+
+
+    }
+    render() {
+        return (
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event starts in
+                </div>
+                <div className="countdown_bottom">
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            23
+                        </div>
+                        <div className="countdown_tag">
+                            Days
+                            </div>
+                            <div className="countdown_item">
+                        <div className="countdown_time">
+                            2
+                        </div>
+                        <div className="countdown_tag">
+                            hs
+                            </div>
+                            <div className="countdown_item">
+                        <div className="countdown_time">
+                            20
+                        </div>
+                        <div className="countdown_tag">
+                            Min
+                            </div>
+                            <div className="countdown_item">
+                        <div className="countdown_time">
+                            40
+                        </div>
+                        <div className="countdown_tag">
+                            Sec
+                            </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TimeUntil;
