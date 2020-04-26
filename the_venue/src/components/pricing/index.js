a39cc591d26b231cb1d03a0818173e0fe0fb96b7
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-25 19:12:23 -0700

    adding .map method map array onto new div classes

--- the_venue/src/components/pricing/index.js
+++ the_venue/src/components/pricing/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class Pricing extends Component {
+
+    state = {
+        prices: [100, 150, 250],
+        positions: ["Balcony", "Medium", "Star"],
+        desc: [
+            "hi this is all temporary text blah blah blah blah",
+            "hi this is more text baskdasjda",
+            "even more text fahsdasjdnasjdnas"
+        ]
+    }
+
     render() {
         return (
             <div className="bck_black">
                 <div className="center_wrapper pricing_section">
                     <h2>Pricing</h2>
-                </div>        
+                <div className="pricing_wrapper">
+                    boxes
+                </div>
+                 </div>        
             </div>
         );
     }
