1aa36614e85c7aa40a0ffa2881c1e6bc71e2ed0d
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-26 20:10:59 -0700

    adding divs, spans, and button in Zoom tag. Created delay property of state

--- the_venue/src/components/pricing/index.js
+++ the_venue/src/components/pricing/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MyButton from '../utils/MyButton';
+import Zoom from 'react-reveal/Zoom';
 
 class Pricing extends Component {
 
@@ -12,24 +13,32 @@ class Pricing extends Component {
             "even more text fahsdasjdnasjdnas"
         ],
         linkto: ["http://sales/b", "https://sales/m", "https://sales/s"],
+        delay: [500, 0, 500]
     }
 
     showboxes = () => (
         this.state.prices.map((box, i) => {
-            <div className="pricing_item">
-                <div className="pricing_inner_wrapper">
-                    <div className="pricing_title">
-        <span>${this.state.prices[i]}</span>
-        <span>${this.state.positions[i]}</span>
-            </div>
-                    <div className="pricing_description">
+            <Zoom key={i} delay={this.state.delay[i]}> 
+                <div className="pricing_item">
+                    <div className="pricing_inner_wrapper">
+                        <div className="pricing_title">
+                            <span>${this.state.prices[i]}</span>
+                            <span>${this.state.positions[i]}</span>
+                        </div>
+                        <div className="pricing_description">
                             {this.state.desc[i]}
-                    </div>
-                    <div className="pricing_buttons">
-                    
+                        </div>
+                        <div className="pricing_buttons">
+                            <MyButton
+                                text="Purchase"
+                                bck="#ffa800"
+                                color="#fffffff"
+                                link={this.state.linkto[i]}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Zoom>
         })
 
     )
@@ -40,13 +49,8 @@ class Pricing extends Component {
                 <div className="center_wrapper pricing_section">
                     <h2>Pricing</h2>
                     <div className="pricing_wrapper">
-                        <MyButton
-                            text="Purchase"
-                            bck="#ffa800"
-                            color="#fffffff"
-                            link={this.state.linkto[i]}
-                        />
-                </div>
+
+                    </div>
                 </div>
             </div>
         );
